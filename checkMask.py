51826d80d4aa8f936c3a07ab24ca036628072289
AdamWang518 <dodoga518@gmail.com>
2024-06-25 20:06:37 +0800

    check

---
+++ checkMask.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+# Set the path to the saved .npy file
+file_path = "final_mask.npy"
+
+# Load the saved mask
+loaded_mask = np.load(file_path)
+
+# Verify the loaded mask
+print("Loaded mask shape:", loaded_mask.shape)
+print("Loaded mask dtype:", loaded_mask.dtype)
+
+# Display the loaded mask
+plt.imshow(cv2.cvtColor(loaded_mask[:, :, :3], cv2.COLOR_BGR2RGB))
+plt.title("Loaded Mask")
+plt.show()
